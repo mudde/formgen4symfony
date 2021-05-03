2594e49cef3780e21396ae7e8beee7d7168b8d46
Olaf Mudde <olaf.mudde@xs4all.nl>
2021-05-04 00:54:22 +0200

    Start with MuddeFormgen4symfonyBundle

---
+++ MuddeFormgen4symfonyBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mudde\Formgen4Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MuddeFormgen4symfonyBundle extends Bundle
+{
+    public function boot()
+    {
+    }
+
+    public function shutdown()
+    {
+    }
+}
