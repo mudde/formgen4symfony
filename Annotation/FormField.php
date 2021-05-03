77c91b3bc0a9926fdfaf7706b344a08463216120
Olaf Mudde <olaf.mudde@xs4all.nl>
2021-05-03 21:49:04 +0200

    Add FormField Annotation

---
+++ Annotation/FormField.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Mudde\Formgen4Symfony\Annotation;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class FormField
+{
+    private array $config = [];
+
+    public function __construct(
+        string $id,
+        string $label,
+        array $validators = [],
+        array $attributes = [],
+    )
+    {
+        $this->config = $attributes;
+        $this->config['id'] = $id;
+        $this->config['label'] = $label;
+        $this->config['validators'] = $validators;
+    }
+
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+}
